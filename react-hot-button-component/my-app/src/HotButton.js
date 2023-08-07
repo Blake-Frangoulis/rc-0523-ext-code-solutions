33a13a9360ac7808bf77faa378ddc97a31a44049
Blake Frangoulis <blakef44@gmail.com>
2023-08-07 21:51:50 +0000

    changed button text color after reaching fourth stage

--- react-hot-button-component/my-app/src/HotButton.js
+++ react-hot-button-component/my-app/src/HotButton.js
@@ -3,6 +3,7 @@ import { useState } from 'react';
 export default function HotButton() {
   const [clicks, setClicks] = useState(1);
   const [color, setColor] = useState('blue');
+  const [textColor, setTextColor] = useState('white');
 
   function handleClicks() {
     setClicks(clicks + 1);
@@ -12,6 +13,7 @@ export default function HotButton() {
       setColor('yellow');
     } else if (clicks <= 12 && clicks > 9) {
       setColor('orange');
+      setTextColor('black');
     } else if (clicks <= 9 && clicks > 6) {
       setColor('red');
     } else if (clicks <= 6 && clicks > 3) {
@@ -22,7 +24,9 @@ export default function HotButton() {
   }
 
   return (
-    <button onClick={handleClicks} style={{ backgroundColor: color }}>
+    <button
+      onClick={handleClicks}
+      style={{ backgroundColor: color, color: textColor }}>
       Hot Button
     </button>
   );
